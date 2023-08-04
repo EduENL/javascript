5d78906d68c45e250e160bbf0316b57173571d21
EduENL <eduardonuneslima099@gmail.com>
2023-08-04 16:08:16 -0300

    ate_string
    Att até string

---
+++ exemplos/2_TIPOS_DE_DADOS/4_string/script.js
@@ -0,0 +1,8 @@
+console.log("Meu nome é Eduardo");
+console.log('Desenvolvimento é no Senai');
+
+// Template literais (crase)
+console.log(`teste`);
+
+console.log(typeof "Infinity")
+console.log(typeof Infinity)
